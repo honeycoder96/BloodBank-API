0f5f4adb61f680f7ec0b3a0ba0214516fd67f382
honeycoder96 <softwaretester2016pro@gmail.com>
2018-03-02 01:17:50 +0530

    initial commit

---
+++ src/main/java/tech/honeysharma/techbme/TechbMeApplication.java
@@ -0,0 +1,12 @@
+package tech.honeysharma.techbme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbMeApplication.class, args);
+	}
+}
